c3bc9d844a93bbe6fc08c33329813b415563a89d
HemanKoli <hemankoli1409@gmail.com>
2025-09-30 09:50:43 +0530

    refactor: Remove authentication middleware from user and admin routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const userController = require('../controllers/userController');
-const { checkAuthenticated, isAdmin } = require("../middleware/middleware");
 
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 router.post('/logout', userController.logout);
 router.get('/all-users', userController.getAllUsers);
 
-router.get("/user-auth",  checkAuthenticated, (req, res) => {
+router.get("/user-auth",  (req, res) => {
     res.status(200).send({ ok: true, user: req.user });
 });
 
-router.get("/admin-auth", checkAuthenticated, isAdmin, (req, res) => {
+router.get("/admin-auth", (req, res) => {
     res.status(200).send({ ok: true, user: req.user });
 });
 
